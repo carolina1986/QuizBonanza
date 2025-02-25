c313a932788d9e264105edff0c03068cb8579a23
CarolinaHoglund <carolinagok1@gmail.com>
2025-02-25 22:00:29 +0100

    Created IRepository with basic CRUD operations

---
+++ QuizApp/Repositories/IRepository.cs
@@ -0,0 +1,38 @@
+// This interface is used to define the generic repository pattern.
+// It contains the basic CRUD operations that are used in the repository pattern.
+// The repository pattern is used to separate the data access logic from the business logic.
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace QuizzApp.Repositories
+{
+
+    // 'where T : class' constrains the generic type T to reference (class) types only 
+    public interface IRepository<T> where T : class
+    {
+        // This method will return the entity with the given id
+        T GetById(int id);
+
+        // IEnumarable is used so we can have a collection of entities (e.g. a list of users)
+        IEnumerable<T> GetAll(); 
+
+        // This method will find all the entities that match the given expression (e.g. all users with a specific name)
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression); 
+        
+        // 'T entity' is the entity that we want to add to the database
+        void Add(T entity);
+
+        // Using IEnumerable so we can add multiple entities to the database (e.g. a list of questions)
+        void AddRange(IEnumerable<T> entities);
+
+        void Update(T entity); 
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+
+        void SaveChanges();
+    }
+}
--- QuizApp/obj/Debug/net9.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/obj/Debug/net9.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b53bb434ca34d4dccd70dc1f166592379592be7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a4e54010db433845761d2516cfb95e4d83dabe")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
