e683b11711d6a2fc5bd984c084d9e19807a03602
CarolinaHoglund <carolinagok1@gmail.com>
2025-02-26 10:39:26 +0100

    update to .NET v. 8

--- QuizApp/obj/Debug/net9.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/obj/Debug/net9.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a4e54010db433845761d2516cfb95e4d83dabe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c313a932788d9e264105edff0c03068cb8579a23")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
