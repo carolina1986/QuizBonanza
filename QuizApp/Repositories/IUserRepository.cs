5abbee764ef60941ff5b01b1b289f6d98c9a7bdf
CarolinaHoglund <carolinagok1@gmail.com>
2025-02-27 10:16:23 +0100

    Create IUserRepository

---
+++ QuizApp/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+using QuizApp.Models;
+
+namespace QuizApp.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        // This method (only signature) is here because it is specific to the User entity
+        User GetByUsername(string username);
+    }
+}
