e1c46457c795bf5ea72b2ef729ea5565b03cca89
CarolinaHoglund <carolinagok1@gmail.com>
2025-02-28 08:14:13 +0100

    Create IQuestionRepository

---
+++ QuizApp/Repositories/IQuestionRepository.cs
@@ -0,0 +1,11 @@
+using QuizApp.Models;
+
+namespace QuizApp.Repositories
+{
+    public interface IQuestionRepository : IRepository<Question>
+    {
+        Question GetQuestionWithOptions(int questionId);
+        IEnumerable<Question> GetQuestionsByQuizId(int quizId);
+        IEnumerable<Question> GetQuestionsWithOptionsByQuizId(int quizId);
+    }
+}
