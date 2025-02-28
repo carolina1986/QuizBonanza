cef9f4ae626159c038dc79d2109974d85448ece9
CarolinaHoglund <carolinagok1@gmail.com>
2025-02-28 15:02:18 +0100

    Add GetQuizzesByCreator

--- QuizApp/Repositories/IQuizRepository.cs
+++ QuizApp/Repositories/IQuizRepository.cs
@@ -6,5 +6,6 @@ namespace QuizApp.Repositories
     {
         Quiz GetQuizWithQuestions(int quizId);
         Quiz GetQuizWithQuestionsAndOptions(int quizId);
+        IEnumerable<Quiz> GetQuizzesByCreator(int creatorId);
     }
 }
