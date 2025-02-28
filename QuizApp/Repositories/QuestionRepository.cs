86ccd520a650a0223584eb0296d2e3cff7abecd3
CarolinaHoglund <carolinagok1@gmail.com>
2025-02-28 08:14:27 +0100

    Create QuestionRepository

---
+++ QuizApp/Repositories/QuestionRepository.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using QuizApp.Models;
+using QuizApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuizApp.Repositories 
+{
+    public class QuestionRepository : Repository<Question>, IQuestionRepository
+    {
+        public QuestionRepository(QuizDbContext context) : base(context)
+        {
+        }
+
+        public Question GetQuestionWithOptions(int questionId)
+        {
+            return _context.Questions // This will return all the questions in the database
+                .Include(q => q.Options) // This will include the options for each question
+                .FirstOrDefault(q => q.Id == questionId); // This will return the question with the given id
+        }
+
+        public IEnumerable<Question> GetQuestionsByQuizId(int quizId)
+        {
+            return _context.Questions
+                .Where(q => q.QuizId == quizId) // This will return all the questions with the given quiz id
+                .ToList(); // This will return the questions as a list (SQL JOIN)
+        }
+
+        public IEnumerable<Question> GetQuestionsWithOptionsByQuizId(int quizId)
+        {
+            return _context.Questions
+                .Include(q => q.Options)
+                .Where(q => q.QuizId == quizId)
+                .ToList();
+        }
+    }
+}
