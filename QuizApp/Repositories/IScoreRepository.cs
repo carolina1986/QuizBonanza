cab3047277bb7a61ff3e42dc5a24ef3dbc53c6a3
CarolinaHoglund <carolinagok1@gmail.com>
2025-02-28 10:40:15 +0100

    Create IScoreRepository

---
+++ QuizApp/Repositories/IScoreRepository.cs
@@ -0,0 +1,15 @@
+using QuizApp.Models;
+
+namespace QuizApp.Repositories
+{
+    public interface IScoreRepository : IRepository<Score>
+    {
+        Score SaveHighScoreToLeaderboard(int userId, int quizId, int points);
+        Score SaveScore(int userId, int quizId, int points);
+        IEnumerable<Score> GetLeaderboardForAllQuizzes();
+        IEnumerable<Score> GetLeaderboardForQuiz(int quizId);
+        IEnumerable<Score> GetLeaderboardForUser(int userId);
+        IEnumerable<Score> GetAllScoresForUser(int userId);
+        Score GetScoreForUserInQuiz(int userId, int quizId);
+    }
+}
