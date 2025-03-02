e9089e8972fc33b9270f5d80ce5e602df209c352
CarolinaHoglund <carolinagok1@gmail.com>
2025-03-02 21:36:55 +0100

    Create the interface for the question service

---
+++ QuizApp/Services/IQuestionService.cs
@@ -0,0 +1,18 @@
+using QuizApp.Models;
+
+namespace QuizApp.Services
+{
+    public interface IQuestionService
+    {
+        Question AddQuestionWithOptionsToQuiz(int quizId, string questionText, List<Option> options);
+        Question GetQuestionById(int id);
+        IEnumerable<Question> GetQuestionsByQuiz(int quizId);
+        void UpdateQuestion(Question question);
+        void UpdateQuestionText(int id, string newQuestionText);
+        void DeleteQuestion(int id);
+        Option AddOptionToQuestion(int questionId, string optionText, bool isCorrect);
+        Option GetOptionById(int optionId);
+        void UpdateOption(int optionId, string newOptionText, bool isCorrect);
+        void DeleteOption(int optionId);
+    }
+}
