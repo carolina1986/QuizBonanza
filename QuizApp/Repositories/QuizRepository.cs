f431b2dfc90436db2f9275defb54782a3719395d
CarolinaHoglund <carolinagok1@gmail.com>
2025-03-09 16:53:13 +0100

    Update QuizRepository

--- QuizApp/Repositories/QuizRepository.cs
+++ QuizApp/Repositories/QuizRepository.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using QuizApp.Models;
 using QuizApp.Data;
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace QuizApp.Repositories
@@ -13,14 +10,14 @@ namespace QuizApp.Repositories
         {
         }
 
-        public Quiz GetQuizWithQuestions(int quizId)
+        public Quiz? GetQuizWithQuestions(int quizId)
         {
             return _context.Quizzes // This will return all the quizzes in the database
                 .Include(q => q.Questions) // This will include the questions for each quiz
                 .FirstOrDefault(q => q.Id == quizId); // This will return the quiz with the given id
         }
 
-        public Quiz GetQuizWithQuestionsAndOptions(int quizId)
+        public Quiz? GetQuizWithQuestionsAndOptions(int quizId)
         {
             return _context.Quizzes 
                 .Include(q => q.Questions)
@@ -32,6 +29,14 @@ namespace QuizApp.Repositories
         {
             return _context.Quizzes
                 .Where(q => q.CreatorId == creatorId)
+                .ToList();
+        }
+
+        public new IEnumerable<Quiz> GetAll()
+        {
+            return _context.Quizzes
+                .Include(q => q.Creator)   // Inkluderar Creator så att Username hämtas
+                .Include(q => q.Questions) // Inkluderar även frågor om du vill ha dem direkt
                 .ToList();
         }
     }
